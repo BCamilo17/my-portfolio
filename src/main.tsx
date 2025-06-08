a583ff51ae3de72489bda2882c5e0959de5389c8
Brayan Annear <annear2000@gmail.com>
2025-06-07 21:22:27 -0500

    ADD::General: add deploy config

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './contexts/themeContext.tsx'
 
-
+// if (window.location.pathname === '/my-portfolio/' && window.location.hostname !== 'localhost') {
+//   window.location.replace('https://BCamilo17.github.io/');
+// }
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
