a89b30af02c33059e2f8ae661ff1b21ff125ae2a
Brayan Annear <annear2000@gmail.com>
2025-06-08 21:37:58 -0500

    Update::General:update deploy to vercel

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div>
-      <Router basename='/my-portfolio'>
+      <Router>
         <Header />
         <Routes>
           <Route path='/' element={<Home />} />
