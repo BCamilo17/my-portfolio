83b234883d3173986afe7ed59dcc18de216252d8
Brayan Annear <annear2000@gmial.com>
2026-02-16 21:43:42 -0500

    feat:(General, experience): updated react version and update experience

--- src/data/experience/experienceData.ts
+++ src/data/experience/experienceData.ts
@@ -1,11 +1,35 @@
 export const experienceData = [
   {
     id: 1,
+    role: "Ingeniero senior",
+    company: "Interrapidísimo",
+    period: {
+      start: "Enero 2026",
+      end: "Actualidad",
+    },
+    description:
+      "Como ingeniero senior, mis responsabilidades se centran en aplicar el desarrollo de soluciones tecnológicas aplicando el marco de trabajo SCRUM. Con el equipo de trabajo coordinamos y ejecutamos los objetivos establecidos para cada sprint, implementando funcionalidades tanto en el backend utilizando tecnologías como Magento y .NET, como en el frontend con Next.js. Colaboro activamente con equipos multidisciplinarios para garantizar la entrega de productos de alta calidad, optimizando procesos y promoviendo las mejores prácticas de desarrollo ágil.",
+    technologies: [
+      "NextJs",
+      "TypeScript",
+      "CSS",
+      "Figma",
+      "SCRUM",
+      "Azure DevOps",
+      "Magento",
+      ".NET",
+      "Material UI",
+      "GraphQL",
+    ],
+    url: "https://interrapidisimo.com/",
+  },
+  {
+    id: 2,
     role: "Desarrollador freelance",
     company: "Quan",
     period: {
       start: "Agosto 2025",
-      end: "Actualidad",
+      end: "Enero 2026",
     },
     description:
       "Como desarrollador web, diseño e implemento plataformas digitales utilizando Angular v17, integrando servicios en el frontend y desarrollando funcionalidades backend. Trabajo bajo la metodología ágil SCRUM y gestiono procesos de CI/CD con Azure DevOps, asegurando un desarrollo eficiente y continuo.",
@@ -20,7 +44,7 @@ export const experienceData = [
     url: "https://www.quan.com.co/",
   },
   {
-    id: 2,
+    id: 3,
     role: "Desarrollador web junior",
     company: "Universidad Ean",
     period: {
@@ -40,8 +64,8 @@ export const experienceData = [
     url: "https://universidadean.edu.co/",
   },
   {
-    id: 3,
-    role: "Desarrollador Web",
+    id: 4,
+    role: "Desarrollador web",
     company: "Aviatur",
     period: {
       start: "Noviembre 2022",
